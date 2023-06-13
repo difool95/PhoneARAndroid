e967ad229c0071ff81190633aef9396f20579208
difool95 <65021359+difool95@users.noreply.github.com>
2023-06-13 11:44:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-// Define constants
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// Set up the renderer
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Add lighting to the scene
 const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 light.position.set(0.5, 1, 0.25);
 scene.add(light);
 
-// Add a cube to the scene
 const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// Create an XR session
+// Create an XR session and set up camera access
 function initXR() {
   if (navigator.xr) {
     navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
@@ -26,41 +22,43 @@ function initXR() {
         navigator.xr.requestSession('immersive-ar').then((session) => {
           renderer.xr.setSession(session);
           session.addEventListener('end', onSessionEnded);
-          renderXRFrame();
+          const glLayer = new XRWebGLLayer(session, renderer);
+          session.updateRenderState({ baseLayer: glLayer });
+          session.requestReferenceSpace('viewer').then((referenceSpace) => {
+            sessionReferenceSpace = referenceSpace;
+            session.requestAnimationFrame(onXRFrame);
+          });
         });
       }
     });
   }
 }
 
-// Render each frame
-function renderXRFrame() {
-  renderer.setAnimationLoop(() => {
-    // Update cube position based on hit test result
-    const hitTestSource = renderer.xr.getHitTestSource();
-    const hitTestResults = renderer.xr.getHitTestResults(hitTestSource);
-
-    if (hitTestResults.length > 0) {
-      const hit = hitTestResults[0];
-      cube.position.setFromMatrixPosition(hit.getPose(sessionReferenceSpace).transform);
-    }
-
-    renderer.render(scene, camera);
-  });
-}
-
-// Handle session end
 function onSessionEnded() {
   renderer.xr.setSession(null);
   renderer.setAnimationLoop(null);
 }
 
-// Start the XR application
-function startAR() {
-  initXR();
+function onXRFrame(time, frame) {
+  const session = frame.session;
+  const pose = frame.getViewerPose(sessionReferenceSpace);
+  if (pose) {
+    const view = pose.views[0];
+    const viewport = session.renderState.baseLayer.getViewport(view);
+    renderer.setSize(viewport.width, viewport.height);
+    camera.matrix.fromArray(view.transform.inverse.matrix);
+    camera.projectionMatrix.fromArray(view.projectionMatrix);
+    const hitTestSource = renderer.xr.getHitTestSource();
+    const hitTestResults = frame.getHitTestResults(hitTestSource);
+    if (hitTestResults.length > 0) {
+      const hit = hitTestResults[0];
+      cube.position.setFromMatrixPosition(hit.getPose(sessionReferenceSpace).transform);
+    }
+    renderer.render(scene, camera);
+  }
+  session.requestAnimationFrame(onXRFrame);
 }
 
-// Resize the renderer on window resize
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -68,4 +66,4 @@ function onWindowResize() {
 }
 
 window.addEventListener('resize', onWindowResize);
-startAR();
+initXR();
