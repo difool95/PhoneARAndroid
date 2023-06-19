df6c3d941f64fd3bcdca910f39e8a2b492407822
difool95 <65021359+difool95@users.noreply.github.com>
2023-06-19 10:53:22 +0100

    Update renderer.js

--- js/render/core/renderer.js
+++ js/render/core/renderer.js
@@ -45,7 +45,7 @@ export const ATTRIB_MASK = {
 const GL = WebGLRenderingContext; // For enums
 
 const DEF_LIGHT_DIR = new Float32Array([-1.0, -1.0, -1.0]);
-const DEF_LIGHT_COLOR = new Float32Array([15.0, 15.0, 15.0]);
+const DEF_LIGHT_COLOR = new Float32Array([7.0, 7.0, 7.0]);
 
 const PRECISION_REGEX = new RegExp('precision (lowp|mediump|highp) float;');
 
