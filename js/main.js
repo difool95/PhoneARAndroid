b9053d46cb92a02425ba39dcb4bfc5cc1dad256a
difool95 <65021359+difool95@users.noreply.github.com>
2023-06-20 16:10:52 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ let object;
 let controls;
 
 //Set which object to render
-let objToRender = 'eye';
+let objToRender = 'phone';
 
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
